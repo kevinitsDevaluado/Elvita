225705c1e18e787817175647ff194c39ffe0ede5
Kevin Alvarado <kevin.alvarado8502@utc.edu.ec>
2020-09-20 17:29:09 -0500

    credenciales de posgrest heroku

--- config/settings/production.py
+++ config/settings/production.py
@@ -8,7 +8,21 @@ ALLOWED_HOSTS = ['devcenter.heroku.com']
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = db.SQLITE
+# psycopg2
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'deaefe7cdvrohv',
+        'USER': 'puvivbqxsyuzgw',
+        'PASSWORD': '387152efef718090bdd168d6d0a0ce9ac0fcc72c495fbadbe1c1255d7583d26c',
+        'HOST': 'ec2-52-22-216-69.compute-1.amazonaws.com',
+        'PORT': '5432',
+        'OPTIONS': {
+            'options': '-c search_path=prueba'
+        },
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
