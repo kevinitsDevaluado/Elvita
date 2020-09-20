86787a0f938d0236cceb64a37d350b78a0e22501
Kevin Alvarado <kevin.alvarado8502@utc.edu.ec>
2020-09-20 17:09:18 -0500

    correccion de host

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,7 +3,7 @@ import config.db as db
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['devcenter.heroku.com']
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
