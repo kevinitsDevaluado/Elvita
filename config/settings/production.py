6bd9bfa89f9a8d6b5223b0c11b58ca51877e21e6
Kevin Alvarado <kevin.alvarado8502@utc.edu.ec>
2020-09-22 20:22:47 -0500

    cambie el host del manage.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,7 +3,7 @@ import config.db as db
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['devcenter.heroku.com']
+ALLOWED_HOSTS = ['inventarioelvita.herokuapp.com']
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
