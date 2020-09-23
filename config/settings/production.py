b93e1b55cd16e7eca85ff68f9be7dd4dc84127ba
Kevin Alvarado <kevin.alvarado8502@utc.edu.ec>
2020-09-22 21:43:49 -0500

    ojala valgan los statics.

---
+++ config/settings/production.py
@@ -0,0 +1,23 @@
+from .base import *
+import config.db as db
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['inventarioelvita.herokuapp.com']
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+# psycopg2
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'deaefe7cdvrohv',
+        'USER': 'puvivbqxsyuzgw',
+        'PASSWORD': '387152efef718090bdd168d6d0a0ce9ac0fcc72c495fbadbe1c1255d7583d26c',
+        'HOST': 'ec2-52-22-216-69.compute-1.amazonaws.com',
+        'PORT': 5432,
+        
+    }
+}
