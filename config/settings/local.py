90b3f24c5becaaaefe38da3df351dfee9aecf45f
Kevin Alvarado <kevin.alvarado8502@utc.edu.ec>
2020-09-22 22:07:04 -0500

    ojala valgan los statics.

---
+++ config/settings/local.py
@@ -0,0 +1,16 @@
+#from .base import *
+#import config.db as db
+
+# SECURITY WARNING: don't run with debug turned on in production!
+#DEBUG = True
+
+#ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+#DATABASES = db.SQLITE
+
+# Password validation
+# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
+
